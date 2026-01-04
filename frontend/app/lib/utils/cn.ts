69f874b660cdb074b51ef2e31e233da0e6b12e97
seino914 <tonosaki914@icloud.com>
2026-01-04 15:31:07 +0900

    cn, sonner追加

---
+++ frontend/app/lib/utils/cn.ts
@@ -0,0 +1,11 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+/**
+ * クラス名を結合するユーティリティ関数
+ * shadcn/uiのコンポーネントで使用
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
