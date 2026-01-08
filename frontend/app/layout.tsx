3c59ebc719cbeac9280c4c082fb146f04073c5e7
seino914 <tonosaki914@icloud.com>
2026-01-09 03:28:27 +0900

    ユーザー登録画面でのヘッダー表示を削除

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { HeaderMenu } from "./components/header/HeaderMenu";
 import { Toaster } from "./components/ui/sonner";
 
 const geistSans = Geist({
@@ -29,7 +28,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <HeaderMenu />
         {children}
         <Toaster />
       </body>
