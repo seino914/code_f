38e45f847b60d1fbe9ce14dec0fb20519442674b
seino914 <tonosaki914@icloud.com>
2026-02-01 20:14:43 +0900

    不要なインポートを削除

--- backend/src/domain/users/users.module.ts
+++ backend/src/domain/users/users.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../database/prisma/prisma.module';
-import { AuthModule } from '../auth/auth.module';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
 import { UsersUsecase } from './usecase/users.usecase';
@@ -11,7 +10,7 @@ import { UsersRepository } from './repository/users.repository';
  * ユーザー情報管理機能を提供
  */
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule],
   controllers: [UsersController],
   providers: [UsersRepository, UsersUsecase, UsersService],
   exports: [UsersService, UsersRepository],
